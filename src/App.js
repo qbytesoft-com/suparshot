1e98bae94cbb077a02bb40e384158c279c72861b
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-01-13 21:15:12 +0600

    Clean up the environment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="white-text">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen width-screen flex flex-row justify-center items-center">
+      <h1 className="text-5xl font-medium">Welcome to Suparshot.com</h1>
     </div>
   );
 }
